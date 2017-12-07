a9fabc709f75d6d0d90768830c8afd1869e23d38
Tyler Morrison <tylermorrison@ebadmins-MacBook-Pro.local>
2017-12-07 15:50:42 -0500

    Finished firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,3 +30,14 @@ export const ROUTES: Routes = [];
   ]
 })
 export class AppModule {}
+
+/*
+* var config = {
+    apiKey: "AIzaSyBbk43IosWkKrLBai4MtOAmMkeV2vTrM5I",
+    authDomain: "fitness-app-56ba9.firebaseapp.com",
+    databaseURL: "https://fitness-app-56ba9.firebaseio.com",
+    projectId: "fitness-app-56ba9",
+    storageBucket: "fitness-app-56ba9.appspot.com",
+    messagingSenderId: "629632761085"
+  };
+* */
