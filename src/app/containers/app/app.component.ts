e0257006afb84ee2fb317272a135f63909af3f9a
Tyler Morrison <tylermorrison@ebadmins-MacBook-Pro.local>
2017-12-11 10:56:10 -0500

    Finished Reactive Store

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Store} from "store";
+import {AuthService, User} from "../../../auth/shared/services/auth/auth.service";
+import {Observable} from "rxjs/Observable";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
+      <h1>{{ user$ | async | json }}</h1>
       <div class="wrapper">
         <router-outlet></router-outlet>
       </div>
     </div>
   `
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy{
+
+  user$: Observable<User>;
+  subscription: Subscription;
+
+  constructor(
+    private authService: AuthService,
+    private store: Store
+  ) {}
+
+  ngOnInit(): void {
+    this.subscription = this.authService.auth$.subscribe();
+    this.user$ = this.store.select<User>('user');
+  }
+
+  // If app component ever gets destroyed, we want to have this just to be sure.
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,11 +1,38 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from "angularfire2/auth";
+import {Store} from "store";
+import "rxjs/add/operator/do";
+
+export interface User {
+  email: string;
+  uid: string;
+  authenticated: boolean;
+}
 
 @Injectable()
 export class AuthService {
+  // Whenever there is a change in AF authState, the following side effects will be invoked
+  auth$ = this.af.authState
+    .do(next => {
+      // On logout, remove existing user data from Store...
+      if(!next) {
+        this.store.set('user', null);
+        // Once we have nulled the user, exit function...
+        return;
+      }
+
+      // On login, set user property of Store...
+      const user: User = {
+        email: next.email,
+        uid: next.uid,
+        authenticated: true
+      };
+      this.store.set('user', user);
+    });
 
   constructor(
-    private af: AngularFireAuth
+    private af: AngularFireAuth,
+    private store: Store
   ) {}
 
   createUser(email: string, password: string) {
